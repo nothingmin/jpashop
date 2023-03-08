d3f7f7d1e3f8f318437a8f8874080eb3cff5f00c
unknown <wnl1012@naver.com>
2023-03-09 01:46:42 +0900

    implement associations

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -9,37 +9,5 @@ public class Item {
     private Long id;
     private String name;
     private int price;
-    private int stockQuanriry;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getStockQuanriry() {
-        return stockQuanriry;
-    }
-
-    public void setStockQuanriry(int stockQuanriry) {
-        this.stockQuanriry = stockQuanriry;
-    }
+    private int stockQuantity;
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name ="ORDERS")
@@ -10,9 +12,18 @@ public class Order {
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems= new ArrayList<>();
+
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
     public Long getId() {
         return id;
     }
@@ -21,13 +32,6 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
 
     public LocalDateTime getOrderDate() {
         return orderDate;
